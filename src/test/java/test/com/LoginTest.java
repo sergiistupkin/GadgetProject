0b4440ced396e4533885411cf70773d2e96d88ff
sergiistupkin <sergeystupkin@gmail.com>
2018-04-06 23:13:30 -0500

    first

---
+++ src/test/java/test/com/LoginTest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author Sergii
+ */
+public class LoginTest {
+    static WebDriver driver;
+    String title;
+    
+    public LoginTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        
+
+        //executes only once before all testes
+        System.setProperty("webdriver.chrome.driver", "d:\\qa\\data\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://qa-id.mybenefitexpress.com//account//login");
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+        //driver.quit();
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void testopenpage() {
+    
+    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    assertNotNull(driver);
+    }
+    
+    @Test
+    public void TestLoginPassword() {
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    
+    WebElement element1=driver.findElement(By.id("Username"));
+    //assertNotNull(element1);
+    element1.sendKeys("sandra123");
+        
+    WebElement element2=driver.findElement(By.id("Password"));
+   // assertNotNull(element2);
+    element2.sendKeys("test");
+    
+    WebElement element3=driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[1]/div[1]/div[1]/div/form/div[4]/button"));
+    assertNotNull(element3);
+    element3.submit();
+        
+    }
+    
+    }
